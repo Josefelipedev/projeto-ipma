636b737c01a827f4585b8f791014c8a3c0bace68
josefelipedev <josefelipedev@gmail.com>
2025-06-13 19:55:08 -0300

    back-end:first commit

---
+++ back-end/src/config/config.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService as NestConfigService } from '@nestjs/config';
+
+@Injectable()
+export class ConfigService {
+  constructor(private configService: NestConfigService) {}
+
+  baseUrlIpma(): string {
+    const baseUrl = this.configService.get<string>('BASE_URL_IPMA');
+    if (!baseUrl) {
+      throw new Error(
+        'BASE_URL_IPMA is not defined in the environment variables.'
+      );
+    }
+    return this.configService.get<string>('BASE_URL_IPMA');
+  }
+}
